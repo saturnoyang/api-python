2fc5bcb243ebc798a1a52301920a17cf15ce553a
saturnoyang <63875736+saturnoyang@users.noreply.github.com>
2025-06-21 12:45:21 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
+from api.v1.endpoints import users, events, messaging
 
 # Crea una instancia de la aplicación FastAPI
 app = FastAPI()
 
 # Define un endpoint (ruta) para la raíz de tu API
 @app.get("/")
-async def read_root():
+async def read_root(): # type: ignore
     return {"message": "¡Hola, Mundo!"}
 
-# Puedes añadir otro endpoint si quieres
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None):
-    if q:
-        return {"item_id": item_id, "q": q, "message": f"Obteniendo item {item_id} con query '{q}'"}
-    return {"item_id": item_id, "message": f"Obteniendo item {item_id}"}
 
+# Incluye los routers de cada módulo
+app.include_router(users.router)
+app.include_router(events.router)
+app.include_router(messaging.router)
